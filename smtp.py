837c07944acbe54acf5634772999591811e181f3
happyguyhere <happyguyhere@hotmail.com>
2017-04-13 19:52:49 -0500

    Added email exception handeling

--- smtp.py
+++ smtp.py
@@ -1,15 +1,22 @@
 import smtplib
+from email.mime.text import MIMEText
 
+
+# hotmail class used to send emails
 class hotmail:
-    def __init__(self):
-        self.smtpObj = smtplib.SMTP('smtp-mail.outlook.com', 587)
-        self.smtpObj.ehlo()
-        self.smtpObj.starttls()
-
-        # email account credentials
-        self.smtpObj.login('store_bot@outlook.com', '123JolleyPeople')
-
     def send_email(self, msg):
-        # insert from and to email address
-        self.smtpObj.sendmail('store_bot@outlook.com', 'happyguyhere@hotmail.com', msg)
+        # msg details
+        mime_msg = MIMEText(msg)
+        mime_msg['Subject'] = "Email from store bot"
+        mime_msg['From'] = ""
+        mime_msg['To'] = ""
 
+        try:
+            # email account credentials
+            smtp_obj = smtplib.SMTP('smtp-mail.outlook.com', 587)
+            smtp_obj.ehlo()
+            smtp_obj.starttls()
+            smtp_obj.login('store_bot@outlook.com', '123JolleyPeople')
+            smtp_obj.sendmail('store_bot@outlook.com', 'happyguyhere@hotmail.com', mime_msg.as_string())
+        except smtplib.SMTPException as e:
+            print(e)
